96cec6d49dd1514fb58516df7871a5c8e2305942
patilmanjunathp <98634302+patilmanjunathp@users.noreply.github.com>
2022-07-24 12:17:53 +0530

    Update EmployeeRestController.java

--- src/main/java/com/greatlearning/employee/management/controller/EmployeeRestController.java
+++ src/main/java/com/greatlearning/employee/management/controller/EmployeeRestController.java
@@ -15,13 +15,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.node.IntNode;
+
 import com.greatlearning.employee.management.entity.Employee;
 import com.greatlearning.employee.management.entity.Role;
 import com.greatlearning.employee.management.entity.User;
 import com.greatlearning.employee.management.service.EmployeeService;
 
-import net.bytebuddy.implementation.attribute.AnnotationAppender.Target.OnType;
+
 
 @RestController
 @RequestMapping("/api")
